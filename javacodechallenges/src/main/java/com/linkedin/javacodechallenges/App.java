edbd3012fa3e03dc828f503e8ae69b962478e71f
ConstantinCazacu <133877096+ConstantinCazacu@users.noreply.github.com>
2023-10-19 07:39:48 +0000

    Investing Change solution

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,23 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
     public static double calculateAverageChangeInvested(List<Double> purchases) {
-        return 0;
-    }
+        {if (purchases.isEmpty())
+        {return 0;}}
+List<Double> purchasesRest = new ArrayList<>();
+double sum = 0;
+    {for (Double doubleValue : purchases){
+        int integerValue = (int)Math.ceil(doubleValue);
+        double purchasesRestDouble = integerValue - doubleValue;
+    purchasesRest.add(purchasesRestDouble);
+    
+    sum += purchasesRestDouble;}}{
+
+    return sum/purchasesRest.size() ;}}
+
 
     public static void main(String[] args) {
         List<Double> purchases = List.of(12.38, 38.29, 5.27, 3.21);
